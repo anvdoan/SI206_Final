49cec9a8b1425fb43000e7c85f5e751b7ccea2d0
emkbrown <emkbrown@umich.edu>
2020-12-01 19:43:41 -0500

    artist by genre complete, documentation added

--- LastFM_API.py
+++ LastFM_API.py
@@ -10,17 +10,26 @@ from genderize import Genderize
 API_KEY = '502895a066a65f103a01d4463cf41430'
 API_SECRET = '15a3fe9b112584efb5f7752dae84a1be'
 
+""" Requires: json object
+Modifies: nothing
+Effects: prints json object to console"""
 def json_print(obj):
     #create formatted string of JSON object
     text = json.dumps(obj, sort_keys=True, indent= 4)
     print(text)
 
+""" Requires: string
+Modifies: split_string
+Effects: returns first word separated by whitespace in string"""
 def get_first_name(string):
     #splits name and returns person's first name
     split_string = string.split()
     return split_string[0]
 
-#get top artists by chart, limit number of data pieces and go by page to avoid duplicates
+
+""" Requires: nothing
+Modifies: artist_list, full_json
+Effects: returns json object of all artists, gathered by chart from last.fm API, as well as a list of artist names from the same API calls."""
 def getArtistsbyChart():
     artist_list = list()
     full_json = list()
@@ -41,22 +50,33 @@ def getArtistsbyChart():
 
     return artist_list, full_json
 
-#gets top artists by genre tag, will need to make list of genres and use for loop to call function
-def get_top_artist_by_genre(genre):
-    artist_tag_url = f"http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag={genre}&api_key={API_KEY}&format=json"
-    artist_tag_response = requests.get(artist_tag_url)
-    topArtists = artist_tag_response.json()['topartists']['artist']
+""" Requires: genre_list (list of strings)
+Modifies: artist_list, full_json
+Effects: returns json object of all artists, gathered by genre from last.fm API, as well as a list of artist names from the same API calls."""
+def getArtistsbyGenre(genre_list):
     artist_list = list()
-    for d in topArtists:
-        name = d['name']
-        artist_list.append(name)
-    first_name_list = list()
-    for i in artist_list:
-        first = get_first_name(i)
-        first_name_list.append(first)
-    return first_name_list
+    full_json = list()
+    for genre in genre_list:
+        artist_tag_url = f"http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag={genre}&limit=25&api_key={API_KEY}&format=json"
+        artist_tag_response = requests.get(artist_tag_url)
+        topArtists = artist_tag_response.json()['topartists']['artist']
 
-#take list of names and return genderize json response linked to the artist's full name for ease of adding to database
+        #add to json by genre
+        for artist in topArtists:
+            artist["genre"] = genre
+            full_json.append(artist)
+
+        #add to artist list - will get first names in genderize function, send full names for now
+        for d in topArtists:
+            name = d['name']
+            artist_list.append(name)
+    
+    return artist_list, full_json
+
+
+""" Requires: artist_list (list of strings)
+Modifies: first_name_list, gender_data
+Effects: returns json object of all artists gathered from last.fm API with predicted gender and probability for each artist."""
 def genderize(artist_list):
     first_name_list = list()
     for i in artist_list:
@@ -65,43 +85,54 @@ def genderize(artist_list):
     gender_data = Genderize().get(first_name_list)
     for i in range(len(gender_data)):
         gender_data[i]["name"] = artist_list[i]
+        if gender_data[i]["gender"] == None:
+            gender_data[i]["gender"] = "none"
     return gender_data
 
-#create database
+""" Requires: db_name (string)
+Modifies: cur, conn
+Effects: creates SQLite database and returns connection and cursor"""
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
 
-#set up Gender table in database: holds ids for gender to be connected to gender of artist table with genderize data
+""" Requires: cur, conn (database)
+Modifies: SQLite database
+Effects: creates Genders table in database and adds rows for each gender category from Genderize."""
 def setUpGenderTable(cur, conn):
-    gender_list = ['male', 'female']
+    gender_list = ['male', 'female', 'none']
     cur.execute("DROP TABLE IF EXISTS Genders")
     cur.execute("CREATE TABLE Genders (name TEXT, gender_id INTEGER PRIMARY KEY)")
     for i in range(len(gender_list)):
         if gender_list[i] == "male":
             cur.execute("INSERT INTO Genders (name, gender_id) VALUES (?,?)",(gender_list[i], 0))
-        else:
+        elif gender_list[i] == 'female':
             cur.execute("INSERT INTO Genders (name, gender_id) VALUES (?,?)",(gender_list[i], 1))
+        else:
+           cur.execute("INSERT INTO Genders (name, gender_id) VALUES (?,?)",(gender_list[i], 2)) 
     conn.commit()
 
-#set up artist table with name, playcount, listeners
+""" Requires: data (json obj), cur, conn (database)
+Modifies: SQLite database
+Effects: creates Artists table in database and adds rows for each artist collected from last.fm API."""
 def setUpArtistTable(data, cur, conn):
     cur.execute("DROP TABLE IF EXISTS Artists")
-    cur.execute("CREATE TABLE Artists (artist_id INTEGER PRIMARY KEY, name TEXT, playcount INTEGER, listeners INTEGER)")
+    cur.execute("CREATE TABLE Artists (artist_id INTEGER PRIMARY KEY, name TEXT, genre TEXT)")
     #loop through data and add to database
     i = 0
     for artist in data:
         artist_id = i
         i += 1
         name = artist["name"]
-        pcount = artist["playcount"]
-        listeners = artist["listeners"]
-        cur.execute("INSERT INTO Artists (artist_id, name, playcount, listeners) VALUES (?, ?, ?, ?)", (artist_id, name, pcount, listeners))
+        genre = artist["genre"]
+        cur.execute("INSERT INTO Artists (artist_id, name, genre) VALUES (?, ?, ?)", (artist_id, name, genre))
     conn.commit()
 
-#set up table with artist name and gender as decided by genderize api
+""" Requires: data (json obj), cur, conn (database)
+Modifies: SQLite database
+Effects: creates ArtistGender table in database and adds rows for each artist, their respective predicted gender, and probability of correctness."""
 def setUpArtistGenderTable(data, cur, conn):
     cur.execute("DROP TABLE IF EXISTS ArtistGender")
     cur.execute("CREATE TABLE ArtistGender (artist_id INTEGER PRIMARY KEY, gender_id INTEGER, probability FLOAT)")
@@ -109,12 +140,12 @@ def setUpArtistGenderTable(data, cur, conn):
         cur.execute("SELECT artist_id FROM Artists WHERE name = ?", (item['name'],))
         artist_id = int(cur.fetchone()[0])
         cur.execute("SELECT gender_id FROM Genders WHERE name = ?", (item['gender'],))
-        gender_id = int(cur.fetchone()[0])
+        gender_id = int(cur.fetchone()[1])
         prob = item['probability']
         cur.execute("INSERT INTO ArtistGender  (artist_id, gender_id, probability) VALUES (?, ?, ?)", (artist_id, gender_id, prob))
 
 def main():
-    name_list, json_data = getArtistsbyChart()
+    name_list, json_data = getArtistsbyGenre(['rock', 'pop', 'folk', 'rnb', 'singer-songwriter', 'indie'])
     gender_data = genderize(name_list)
     cur, conn = setUpDatabase('lastfm.db')
     setUpArtistTable(json_data, cur, conn)
