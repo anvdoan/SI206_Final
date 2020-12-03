dd5e9a7806fa15e0c041fd1575f314f9075d1dc8
emkbrown <emkbrown@umich.edu>
2020-12-03 13:01:20 -0500

    started calculations file

--- calculations.py
+++ calculations.py
@@ -8,3 +8,26 @@ import os
 #SI 206 Final Project FA2020
 #Calculations and Visualizations
 
+""" Requires: db_name (string)
+Modifies: cur, conn
+Effects: connects to SQLite database and returns connection and cursor"""
+def connectToDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def gatherData(cur, conn, genres):
+    data_dict = {}
+    return data_dict
+
+
+def main():
+    genres = ['rock', 'pop', 'folk', 'rnb', 'singer-songwriter', 'indie']
+    cur, conn = connectToDatabase('lastfm.db')
+    data_dict = gatherData(cur, conn, genres)
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
